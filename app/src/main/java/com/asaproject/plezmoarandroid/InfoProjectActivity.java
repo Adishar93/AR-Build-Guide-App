fd734d2bc29e99afd4a6e97b548d65f58e2ffb4b
a_man <aman.j.shrivastava@gmail.com>
2019-02-20 01:20:29 +0530

    Recentproject Activity and info activity

---
+++ app/src/main/java/com/asaproject/plezmoarandroid/InfoProjectActivity.java
@@ -0,0 +1,13 @@
+package com.asaproject.plezmoarandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InfoProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_project);
+    }
+}
---
+++ app/src/main/java/com/asaproject/plezmoarandroid/RecentProjectsActivity.java
@@ -0,0 +1,35 @@
+package com.asaproject.plezmoarandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+
+public class RecentProjectsActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    int[] mPlaceList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.recyclerview);
+        GridLayoutManager mGridLayoutManager = new GridLayoutManager(RecentProjectsActivity.this, 2);
+        mRecyclerView.setLayoutManager(mGridLayoutManager);
+
+        mPlaceList = new int[]{R.drawable.image_1, R.drawable.image_2, R.drawable.image_3,
+                R.drawable.image_4, R.drawable.image_5, R.drawable.image_6, R.drawable.image_7,
+                R.drawable.image_8, R.drawable.image_9, R.drawable.image_10};
+
+        RecentProjectAdapter myAdapter = new RecentProjectAdapter(RecentProjectsActivity.this, mPlaceList);
+        mRecyclerView.setAdapter(myAdapter);
+    }
+}
+
+
+
