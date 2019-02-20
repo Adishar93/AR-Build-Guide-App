d0fee0e990087c2557481ffb0eae2e67fe0ef2c0
bichpuriyashruti <47749102+bichpuriyashruti@users.noreply.github.com>
2019-02-20 21:20:16 +0530

    1.mascot and bubble added 2.OnClick of support button redirected to plezmo website

--- app/src/main/java/com/asaproject/plezmoarandroid/MainActivity.java
+++ app/src/main/java/com/asaproject/plezmoarandroid/MainActivity.java
@@ -1,10 +1,12 @@
 package com.asaproject.plezmoarandroid;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageButton settings= (ImageButton) findViewById(R.id.settings);
+        ImageButton mascot= (ImageButton) findViewById(R.id.mascot);
+
     }
+
     public void openSettings(View v){
         Intent i=new Intent(this,SettingsActivity.class);
         startActivity(i);
+    }
+
+    public void onMascotClick(View v){
+        ImageView im=(ImageView) findViewById(R.id.bubble);
+        int i=im.getVisibility();
+        if(i==4)
+        im.setVisibility(ImageView.VISIBLE);
+        else
+            im.setVisibility(ImageView.INVISIBLE);
     }
 }
--- app/src/main/java/com/asaproject/plezmoarandroid/SettingsActivity.java
+++ app/src/main/java/com/asaproject/plezmoarandroid/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.asaproject.plezmoarandroid;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -9,5 +12,12 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settingspage);
+
+    }
+    public void onAboutClicked(View v) {
+        String url = "https://www.plezmo.com";
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse(url));
+        startActivity(i);
     }
 }
